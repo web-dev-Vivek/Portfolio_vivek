8758e8c3911dcb90720bd38f08cb0103a3edc0db
Vivek Jha <progamervivek2020@gmail.com>
2025-07-19 21:30:16 +0530

    .

--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Page3() {
   return (
     <div>
-      <div className="w-screen h-[100vh] flex bg-[rgb(86,84,84)] text-white md:bg-white md:text-black">
+      <div className="w-screen h-[55vh] flex bg-[rgb(242,242,242)] text-black">
         <div className="w-full h-fit flex flex-col items-start justify-start px-10 py-20">
           <svg
             className="o-ui-arrow mb-6 w-10 h-10 md:w-16 md:h-16"
@@ -27,9 +27,14 @@ function Page3() {
             />
           </svg>
 
-          <div className="text-2xl md:text-3xl pl-10 w-full md:w-[30vw]">
+          <div
+            style={{ fontFamily: "Oswald, sans-serif" }}
+            className="text-2xl md:text-4xl pl-10 md:pl-30 w-full md:w-[40vw]"
+          >
             Web & mobile / UI&Ux / Branding / Front-end Development
           </div>
+
+          <div className="w-[80vw] m-auto h-40 border-b border-gray-300"></div>
         </div>
       </div>
     </div>
--- src/pages/page4.jsx
+++ src/pages/page4.jsx
@@ -1,13 +1,53 @@
 import React from "react";
 
-function page4() {
+function Page4() {
   return (
     <div>
-      <div className="w-screen h-[100vh] flex justify-center items-center">
-        hello
+      <div className="w-screen h-[100vh] flex justify-center items-center bg-[rgb(242,242,242)]">
+        <div className="w-screen flex justify-evenly items-center h-40 ">
+          <div
+            style={{ fontFamily: "Oswald, sans-serif" }}
+            className="md:text-8xl font-bold"
+          >
+            FEAT WO
+            <span
+              className="text-9xl"
+              style={{
+                fontFamily: "Bubblegum Sans, cursive",
+                color: "rgb(20, 207, 147)",
+              }}
+            >
+              r
+            </span>
+            KS
+          </div>
+          <div>
+            <svg
+              className="o-ui-arrow mb-6 w-10 h-10 md:w-16 rotate-90 md:h-16"
+              viewBox="0 0 64 64"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.10162 3.10156L62.9999 62.9999"
+                stroke="gray"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M63 1.00001L63 63L0.999989 63"
+                stroke="gray"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default page4;
+export default Page4;
